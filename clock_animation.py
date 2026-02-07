72cb824d0e3160d96cb4b206f7e571ae037cf6a1
Thien an Trieu <156458217+thienantrieu@users.noreply.github.com>
2026-02-07 18:24:20 +0200

    Modify clock_animation.py to include seamaster
    Added seamaster import and commented out alternative animation.

--- clock_animation.py
+++ clock_animation.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import lateksii
+import seamaster
 
 if __name__ == '__main__':
-    fig,ax = plt.subplots()
+    fig = plt.figure() 
     animation = FuncAnimation(fig, lateksii.clock, 1)
+    # animation = FuncAnimation(fig, seamaster.clock, 1)
     plt.show()
-
